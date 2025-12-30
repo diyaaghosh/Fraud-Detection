24729cc0426d70676f160646a1e12443e5d31e75
Diya Ghosh <diya1692005ne@gmail.com>
2025-12-30 22:52:28 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -106,6 +106,7 @@ def predict():
         "reasons": reasons
     })
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000)
+
 
